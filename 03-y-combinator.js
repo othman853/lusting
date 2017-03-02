5782ee428c70c56e0c8ab4cd0c4a36efa3446687
othman853 <anuar.yasser@gmail.com>
2017-03-02 05:49:46 -0300

    Trying to understand y combinator

--- 03-y-combinator.js
+++ 03-y-combinator.js
@@ -1,5 +1,18 @@
 const YCombinatorFactorial =
   ((y => y(givenFact => n => n < 2 ? 1 : n * givenFact(n - 1))(5)) (le => (f => f(f))(f => le(x => (f(f))(x)))));
 
-
 console.log(YCombinatorFactorial);
+
+
+const factorial = givenFact => n => (n < 2) ? 1 : n * givenFact(n - 1);
+
+const firstPiece =
+  y => y(givenFact => n => n < 2 ? 1 : n * givenFact(n - 1));
+
+const secondPiece = (le => (f => f(f)));
+
+const YCombinatorFactorial2 = (
+  (firstPiece(5)) (secondPiece(f => le(x => (f(f))(x))))
+);
+
+console.log(YCombinatorFactorial2);
