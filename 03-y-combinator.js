bc031492a715b58b1fa283f955e57caacb106568
othman853 <anuar.yasser@gmail.com>
2017-03-02 00:51:56 -0300

    Adding y-combinator example

---
+++ 03-y-combinator.js
@@ -0,0 +1,5 @@
+const YCombinatorFactorial =
+  ((y => y(givenFact => n => n < 2 ? 1 : n * givenFact(n - 1))(5)) (le => (f => f(f))(f => le(x => (f(f))(x)))));
+
+
+console.log(YCombinatorFactorial);
